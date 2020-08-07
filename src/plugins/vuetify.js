555ac8709335067428f0a087a76ab09e21be3f4b
Maxime GROSSET <grosset.maxime@gmail.com>
2020-08-07 23:14:23 +0200

    reset to vue2

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,38 @@
+// import Vue from 'vue';
+// import Vuetify, {
+//   VApp,
+//   VBtn,
+//   VAlert,
+// } from 'vuetify/lib';
+
+// Vue.use(Vuetify, {
+//   components: {
+//     VApp,
+//     VBtn,
+//     VAlert,
+//   },
+// });
+
+// const opts = {
+//   theme: {
+//     dark: true,
+//   },
+// };
+
+// export default new Vuetify(opts);
+
+// src/plugins/vuetify.js
+
+// import Vue from 'vue';
+// import Vuetify from 'vuetify';
+// import 'vuetify/dist/vuetify.min.css';
+
+// Vue.use(Vuetify);
+
+// const opts = {
+//   theme: {
+//     dark: true,
+//   },
+// };
+
+// export default new Vuetify(opts);
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 // const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin');
 
 module.exports = {
-  productionTip: false,
+  // productionTip: false,
   css: {
     loaderOptions: {
       scss: {
