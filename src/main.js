51740a37d63999b44a018a94ecd9f71806d224d3
Maxime GROSSET <grosset.maxime@gmail.com>
2020-08-08 17:57:03 +0200

    Add vue-router

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue';
+
+import vuetify from './plugins/vuetify';
+import VueRouter from './plugins/VueRouter';
+
 import App from './App.vue';
 import store from './store';
-import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
 new Vue({
   store,
+  VueRouter,
   vuetify,
   render: (h) => h(App),
 }).$mount('#app');
